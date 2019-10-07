325083a0eccb761298056d48803c1b5b34cf72ce
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2019-10-07 16:16:43 +0330

    namespace for views

--- src/RatechServiceProvider.php
+++ src/RatechServiceProvider.php
@@ -9,7 +9,7 @@ class RatechServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/views');
+        $this->loadViewsFrom(__DIR__ . '/views','');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->publishes([
             __DIR__.'/views' => resource_path('views'),
