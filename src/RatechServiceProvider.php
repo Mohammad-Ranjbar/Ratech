9b32c31fdf75ddd203b2be3f5427f808be0e716a
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2019-10-07 12:53:26 +0330

    delete config

--- src/RatechServiceProvider.php
+++ src/RatechServiceProvider.php
@@ -8,12 +8,11 @@ class RatechServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views','contact');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->mergeConfigFrom(__DIR__.'/config/contact.php', 'contact');
-        $this->publishes([__DIR__.'/config/contact.php' , config_path('contact.php')]);
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/views', 'contact');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
+
     public function register()
     {
     }
--- src/database/migrations/2019_10_07_081505_create_contacts_table.php
+++ src/database/migrations/2019_10_07_081505_create_contacts_table.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Ratech\AdminPanel\database\migrations;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
