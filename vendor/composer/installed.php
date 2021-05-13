2d0d10f184cb5f9e59a7d9a95ba8cf42bc07b1e8
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2021-05-13 11:47:57 +0430

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd7dbb83d491b4712eeb972f74441c30023135ad8',
+    'name' => 'ratech/adminpanel',
+  ),
+  'versions' => 
+  array (
+    'ratech/adminpanel' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd7dbb83d491b4712eeb972f74441c30023135ad8',
+    ),
+  ),
+);
